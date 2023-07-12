3a9a02110fc795ae6c61ad5058747570716a78ec
Devin H. Kehoe <dhkehoe@gmail.com>
2023-07-12 11:57:22 -0400

    added figure handle optional arg

--- fig2pdf.m
+++ fig2pdf.m
@@ -1,8 +1,9 @@
-function fig2pdf(h,filename,dpi)
+function fig2pdf(filename,h,dpi)
 % Save the figure in the figure handle 'h' as a .pdf file named 'filename'
 % with no whitespace padding. Use the resolution (dots-per-inch) specified
 % by the numeric scalar 'dpi' (default = 300).
 if nargin<3, dpi=300; end
+if nargin<2, h=gcf; end
 set(h,'Units','Inches');
 pos = get(h,'Position');
 set(h,'Renderer','painters','PaperUnits','Inches','PaperPositionMode','Manual',...
