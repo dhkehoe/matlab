f1168365be2474d54e1a7374de88c9cdaef6efbb
Devin H. Kehoe <dhkehoe@gmail.com>
2023-12-01 15:23:07 -0500

    converted all args except nr/nc to optional thereby allowing one to specify them with strings (I was always forgetting the order) while also maintaining backwards compatability

--- subplotPos.m
+++ subplotPos.m
@@ -1,28 +1,37 @@
-function pos = subplotPos(nr,nc,lpad,rpad,dpad,upad,adjx,adjy)
+function pos = subplotPos(varargin)
 % This function computes position vectors [left, bottom, width, height] in
 % normalized units to position subplots within a parent figure object. An
 % Nx4 matrix is returned in which the i_th row corresponds to the position
 % vector for i_th subplot, N = (number of rows) x (number of columns), and
-% the subplot rows and columns are linearized.
+% the subplot rows and columns are linearized. Optional arguments are
+% specified using the MATLAB name-pair convention.
+%
+% USAGE
+%   pos = subplotPos(nr,nc);
+%   pos = subplotPos(nr,nc,'OptionalArgName',OptionalArgValue,...);
 %
 % INPUT
 %     nr - Number of subplot rows.
 %
 %     nc - Number of subplot columns.
 %
+% OPTIONAL INPUT
 %   lpad - Magnitude of leftward padding (empty space) to the left of each
 %          subplot.
+%           (default = 0)
 %
 %   rpad - Magnitude of rightward padding (empty space) to the right of
 %          each subplot.
+%           (default = 0)
 %
 %   dpad - Magnitude of downward padding (empty space) to the bottom of
 %          each subplot.
+%           (default = 0)
 %
 %   upad - Magnitude of upward padding (empty space) to the top of each
+%           (default = 0)
 %          subplot.
 %
-% OPTIONAL INPUT
 %   adjx - Magnitude of space at the very bottom of the figure reserved
 %          regardless of the number of rows and the value of 'dpad'. Ideal
 %          for plots with a shared abcissa across vertically stacked
@@ -39,7 +48,8 @@ function pos = subplotPos(nr,nc,lpad,rpad,dpad,upad,adjx,adjy)
 %               (default = 0)
 %
 % OUTPUT
-%  pos - Nx4 matrix containing linearized list of subplot positions
+%  pos - Nx4 matrix containing linearized list of subplot positions where
+%        N = (number of rows) x (number of columns).
 %
 % HISTORY
 % (written)  June 12, 2018
@@ -47,26 +57,38 @@ function pos = subplotPos(nr,nc,lpad,rpad,dpad,upad,adjx,adjy)
 %                           vertically stacked plots.
 % (modified)  Nov 16, 2023: Added 'adjy' parameter to flexibly create
 %                           horizontally stacked plots.
-%
+% (modified)  Dec  1, 2023: Converted arguments to optionals (except nr/nc)numel(x)==1
 %
 %   DHK - June 12, 2018
-if nargin<8, adjy = 0; end
-if nargin<7, adjx = 0; end
 
-pos = nan(nr*nc,4);
-for i = 1:nr % Rows
-    for j = 1:nc % Columns
-        pos((i-1)*nc+j,:) = [ lpad + (j-1)/nc...
-            (nr-i)/nr + dpad...
-            1/nc-lpad-rpad...
-            1/nr-upad-dpad];
+%% Manage inputs
+p = inputParser;
+addRequired(p,'nr',@(x)isnumeric(x)&&isscalar(x)); % 
+addRequired(p,'nc',@(x)isnumeric(x)&&isscalar(x)); % 
+addOptional(p,'lpad',0,@(x)isnumeric(x)&&isscalar(x)); % 
+addOptional(p,'rpad',0,@(x)isnumeric(x)&&isscalar(x)); % 
+addOptional(p,'dpad',0,@(x)isnumeric(x)&&isscalar(x)); % 
+addOptional(p,'upad',0,@(x)isnumeric(x)&&isscalar(x)); % 
+addOptional(p,'adjx',0,@(x)isnumeric(x)&&isscalar(x)); % 
+addOptional(p,'adjy',0,@(x)isnumeric(x)&&isscalar(x)); % 
+parse(p, varargin{:});
+p = p.Results;
+
+%% Routine
+pos = nan(p.nr * p.nc,4);
+for i = 1:p.nr % Rows
+    for j = 1:p.nc % Columns
+        pos((i-1)*p.nc+j,:) = [ p.lpad + (j-1)/p.nc...
+            (p.nr-i)/p.nr + p.dpad...
+            1/p.nc-p.lpad-p.rpad...
+            1/p.nr-p.upad-p.dpad];
     end
 end 
-if adjx
-    pos(:,4) = pos(:,4)-adjx;
-    pos(:,2) = pos(:,2)+reshape(repmat(1:nr,nc,1),[],1)*adjx;
+if p.adjx
+    pos(:,4) = pos(:,4)-p.adjx;
+    pos(:,2) = pos(:,2)+reshape(repmat(1:p.nr,p.nc,1),[],1)*p.adjx;
 end
-if adjy
-    pos(:,3) = pos(:,3)-adjy;
-    pos(:,1) = pos(:,1)+repmat((nc:-1:1)',nr,1)*adjy;
+if p.adjy
+    pos(:,3) = pos(:,3)-p.adjy;
+    pos(:,1) = pos(:,1)+repmat((p.nc:-1:1)',p.nr,1)*p.adjy;
 end
