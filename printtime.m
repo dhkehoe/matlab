132c6c66f0ca929276542602641129fc6cfb0e46
Devin H. Kehoe <dhkehoe@gmail.com>
2024-01-20 19:30:13 -0500

    can give function either seconds or a system time stamp

--- printtime.m
+++ printtime.m
@@ -1,23 +1,17 @@
 function varargout = printtime(t)
-%% manage input
-t = uint64(t(:)); % toc will crash otherwise
+%
+%
+if isa(t,'uint64')
+    t = toc(t);
+end
+t = t(:);
+ft = [floor(t/60^2),floor(rem(t,60^2)/60),rem(t,60)];
 
-%% format time
-ft = nan(numel(t),3);
-for i = 1:numel(t)
-    ft(i,:) = [floor(toc(t(i))/60^2),floor(rem(toc(t(i)),60^2)/60),rem(toc(t(i)),60)];
+if nargout==1
+    % return time formatted as [hrs, mins, seconds.milliseconds; ...]
+    varargout{1} = ft; 
+else
+    % return nothing, print the time
+    fprintf('\nTime elasped (h:mm:ss.ms):\n');
+    fprintf('%d:%02d:%05.2f\n',ft');
 end
-if nargout==1, varargout{1} = ft; return; % return time formatted as [hrs, mins, seconds.milliseconds; ...]
-elseif nargout>1, error('only one return argument for ''printtime''');
-end
-% else, return nothing, print the time
-
-%% print
-fprintf('\nTime elasped (h:mm:ss.ms):\n');
-if numel(t) > 1, fprintf('['); end
-for i =1:numel(t)
-    fprintf('%d:%02d:%05.2f',ft(i,:));
-    if numel(t) > 1 && i<numel(t), fprintf(', '); end
-end
-if numel(t) > 1, fprintf(']'); end
-fprintf('\n');
