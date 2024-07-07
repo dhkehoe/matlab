3b7280c86b37681b506869b37b0df982d62ceb22
Devin H. Kehoe <dhkehoe@gmail.com>
2024-07-06 20:27:57 -0400

    some obvious size operations that matlab doesn't implement and that I'm tired of needing to redefine over and over...

---
+++ eqsize.m
@@ -0,0 +1,5 @@
+function b = eqsize(x,y)
+% Return whether 2 matrices are of equal size along all dimensions
+szx = size(x);
+szy = size(y);
+b = numel(szx)==numel(szy) && all(szx==szy);
---
+++ ndim.m
@@ -0,0 +1,3 @@
+function n = ndim(x)
+% Get the number of dimensions for some data structure
+n = numel(size(x));
