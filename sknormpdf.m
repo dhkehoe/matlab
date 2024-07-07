8e8969f14cee6a59271af6a0d2884e6fd5a54208
Devin H. Kehoe <dhkehoe@gmail.com>
2024-07-06 21:18:46 -0400

    revised for consistency with cdf and inv

--- sknormpdf.m
+++ sknormpdf.m
@@ -1,2 +1,7 @@
 function y = sknormpdf(x,mu,sigma,alpha)
+% The skew normal cumulative distribution function.
+
+% Make it behave like a skewed standard normal if only 2 arguments are
+% given.
+if nargin==2, alpha = mu; mu = 0; sigma = 1; end
 y = 2/sigma .* normpdf((x-mu)./sigma) .* normcdf((x-mu)./sigma.*alpha);
