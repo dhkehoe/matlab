7eccd6be223007bcfe055f2c9d8788dd09c958a7
Devin H. Kehoe <dhkehoe@gmail.com>
2024-07-17 08:49:37 -0400

    initial commit

---
+++ finddim.m
@@ -0,0 +1,19 @@
+function i = finddim(x,dim,varargin)
+% Generalizes MATLAB's  find()  function such that it will return the true
+% indices of 'x' as subscripts along the dimensions specified by 'dim'.
+%
+%
+% EXAMPLE
+%       x = rand(10,4,2);
+%       i = finddim(x,[2,3]);
+% 'i' is an N by M matrix, where N is the number of true elements in 'x'
+% and M is 2. The first column specifies the dimension 2 subscripts of true
+% elements of 'x', while the second column specifies the dimension 3
+% subscripts of true elements of 'x'.
+%
+%
+%   dhk - July 17, 2024
+s = size(x);
+i = find(x(:),varargin{:});
+i = sortrows(ind2submat(s,i));
+i = i(:,dim);
