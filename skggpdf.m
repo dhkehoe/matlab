2e89b89d7f54ee8e25ae2be2f9ce6939e81e0423
dhkehoe <dhkehoe@gmail.com>
2024-08-09 23:34:12 -0400

    needed to update internal function calls

--- skggpdf.m
+++ skggpdf.m
@@ -1,2 +1,2 @@
 function y = skggpdf(x,p)
-y = 2./p(2) .* gennpdf( (x-p(1))/p(2),0,1,p(3) ) .* genncdf( p(4).* (x-p(1))/p(2),0,1,p(3) );
+y = 2./p(2) .* gnormpdf( (x-p(1))/p(2),0,1,p(3) ) .* gnormcdf( p(4).* (x-p(1))/p(2),0,1,p(3) );
