494532fbe0ce7626e9df9e1ce1a52155d7d7cb1c
dhkehoe <dhkehoe@gmail.com>
2024-09-27 20:34:33 -0400

    now much more matlab-plot-like: exactly replicated the argument structure of  hold()

--- latex.m
+++ latex.m
@@ -1,20 +1,44 @@
-function varargout = latex(on)
+function varargout = latex(value)
 % Set the default text interpeter to latex or tex. Tex is the default in
 % MATLAB. When the default is set to latex, this means every text object, 
 % axis label, tick label, etc. will be rendered with the latex interpreter
 % so you won't ever need to use
 %   set(exampleGraphicsHandle,'Interpreter','latex');
 %
+% It accepts arguments exactly like the MATLAB function hold(). When a
+% return value is specified, it behaves exactly like ishold().
+%
 % USAGE
-%   latex; % Default to latex interpreter
-%   latex(1); Default to latex interpreter
-%   latex(0); % Default to tex interpreter
-%   status = latex; % Get interpreter status; does not alter default
+%   % Default interpreter to latex 
+%   latex; 
+%   latex on;
+%   latex('on');
+%   latex("on");
+%   latex(1);
+%   latex(true);
+%
+%   % Default interpreter to tex
+%   latex off;
+%   latex('off');
+%   latex("off");
+%   latex(0);
+%   latex(false);
+%
+%   % Get interpreter status; do not alter current interpreter
+%   status = latex;
 %
 % OPTIONAL INPUT
-%       on - Logical scalar indicating whether to set the default
-%            interpreter to latex (true) or tex (false).
-%               (default = true)
+%    value - Indicate whether to set the default interpreter to latex or
+%            tex. Can be of type
+%                  char:     'on' - sets the interpreter to latex
+%                           'off' - sets the interpreter to tex
+%                string:     "on" - sets the interpreter to latex
+%                           "off" - sets the interpreter to tex
+%               numeric: non-zero - sets the interpreter to latex
+%                            zero - sets the interpreter to tex
+%               logical:     true - sets the interpreter to latex
+%                           false - sets the interpreter to tex
+%            (default = 'on')
 %
 % OUTPUT
 %   status - Logical scalar indicating whether the default interpreter is
@@ -35,12 +59,22 @@ if nargout
     return; % Do not alter default
 end
 
-% Set some default behaviors
+% Set the default behavior
 if nargin<1
-    on = true; % Default to latex (true), if no args
+    value = 1; % Default to latex (true), if no args
+end
+
+% Ensure a valid input; convert to integer
+if isnumeric(value) || islogical(value)
+    value = logical(value)+1;
+else
+    value = find(strcmpi(value,{'off','on'}));
+    if isempty(value)
+        error('Command option must be ''on'' or ''off''.');
+    end
 end
 
 % Set all text interpreters to latex (true) or tex (false)
 opt = {'tex','latex'}; % Interpreter options
-arg = reshape([strrep(p,'factory','Default'),repmat(opt(logical(on)+1),size(p))]',[],1);
+arg = reshape([strrep(p,'factory','Default'),repmat(opt(value),size(p))]',[],1);
 set(groot,arg{:});
