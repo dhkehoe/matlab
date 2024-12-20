2a983104ae6e293c33e3189561eda2829ebc880e
dhkehoe <dhkehoe@gmail.com>
2024-12-20 05:07:59 -0500

    fixed the parameter defaults

--- skggpdf.m
+++ skggpdf.m
@@ -1,2 +1,16 @@
-function y = skggpdf(x,p)
-y = 2./p(2) .* gnormpdf( (x-p(1))/p(2),0,1,p(3) ) .* gnormcdf( p(4).* (x-p(1))/p(2),0,1,p(3) );
+function y = skggpdf(x,mu,sigma,beta,lambda)
+if nargin<5
+    lambda = 0;
+end
+if nargin<4
+    beta = 2;
+end
+if nargin<3
+    sigma = 1;
+end
+if nargin<2
+    mu = 0;
+end
+z = (x-mu)./sigma;
+beta = abs(beta);
+y = 2./sigma .* gnormpdf( z,0,1,beta ) .* gnormcdf( lambda.*z,0,1,beta );
