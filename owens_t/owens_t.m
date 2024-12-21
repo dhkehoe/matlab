8cb4a3e4dc9cee45be210b1059c62c3495097d9e
dhkehoe <dhkehoe@gmail.com>
2024-12-21 01:33:03 -0500

    newer version of boost improved speed better documented

--- owens_t/owens_t.m
+++ owens_t/owens_t.m
@@ -1,4 +1,23 @@
 function y = owens_t(h,a)
-s = size(h);
-if numel(h)==1, s = size(a); end
-y = reshape(owens_t__(h(:),a(:)),s);
+% Owen's T function, T(h, a), gives the probability of the event 
+%   (X > h and 0 < Y < aX)
+% where X and Y are independent standard normal random variables.
+%
+% Defined in
+% Owen, D B (1956). "Tables for computing bivariate normal probabilities".
+%       Annals of Mathematical Statistics, 27, 1075–1090.
+
+% This is a wrapper around a .mex file, which wraps boost::math::owens_t()
+% written in C++.
+% 
+% Compiled under
+%   MSVC    19.40.33813
+%   boost   1.87.0
+
+% .mex linearizes data, so ensure the shape is preserved
+if isscalar(a)
+    s = size(h);
+else
+    s = size(a);
+end
+y = reshape(owens_t__(h,a),s);
--- owens_t/owens_t__.cpp
+++ owens_t/owens_t__.cpp
@@ -1,10 +1,8 @@
-//mex owens_t.cpp__ -I'C:\Program Files\boost\boost_1_80_0'
+// mex instructions:
+// mex owens_t.cpp__ -I'C:\boost_1_87_0' -output owens_t__
 
 #include "mexAdapter.hpp"
-
-#include <iostream>
 #include <stdexcept>
-
 #include <boost/math/special_functions/owens_t.hpp>
 
 class MexFunction : public matlab::mex::Function {
@@ -15,47 +13,43 @@ public:
         if (inputs.size() != 2)
             throw std::invalid_argument( "Must pass 2 arguments" );
 
-        // Cast inputs to arrays
-        const matlab::data::TypedArray<double> h = std::move(inputs[0]);
-        const matlab::data::TypedArray<double> a = std::move(inputs[1]);
+        // Get TypedArray<T> pointers to input arrays
+        matlab::data::TypedArray<double> h = std::move(inputs[0]);
+        matlab::data::TypedArray<double> a = std::move(inputs[1]);
+
+        // Return if either input is empty
+        if ( h.isEmpty() || a.isEmpty() )
+            throw std::invalid_argument( "Empty array passed" );
 
         // Get array sizes
         size_t n = h.getNumberOfElements();
         size_t m = a.getNumberOfElements();
-
-        // Return if either is empty
-        if (!n || !m)
-            throw std::invalid_argument( "Empty array passed" );
         
         // Ensure matching sizes of h and a or that at least one is scalar
-        if (n != m && n != 1 && m != 1)
+        if (  !(n == m || n == 1 || m == 1)  )
             throw std::invalid_argument( "Input array size mismatch" );
 
-        // Allocate memory for outputs
+        // Allocate memory for output
         matlab::data::ArrayFactory f;
         matlab::data::TypedArray<double> y = f.createArray<double>(std::vector<size_t>{std::max(n,m)});
 
+        // To avoid repeated cast in loops, cast pointers:
+        // TypedArray<double> -> unique_ptr<double[]> -> double*
+        double* H = h.release().get();
+        double* A = a.release().get();
+
         // Compute output
-        if (n == m)
-        {
-            for(int i = 0; i < n; i++)
-            {
-                y[i] = (double)boost::math::owens_t((double)h[i], (double)a[i]);
-            }
+        if (n == m) {
+            for(size_t i = 0; i < n; i++)
+                y[i] = boost::math::owens_t(H[i], A[i]);
         }
-        else if (m==1)
-        {
-            for(int i = 0; i < n; i++)
-            {
-                y[i] = boost::math::owens_t((double)h[i], (double)a[0]);
-            }
+        else if (m == 1) {
+            for(size_t i = 0; i < n; i++)
+                y[i] = boost::math::owens_t(H[i], A[0]);
         }
-        else if (n==1)
-        {
-            for(int i = 0; i < m; i++)
-            {
-                y[i] = boost::math::owens_t((double)h[0], (double)a[i]);
-            }
+        else if (n == 1) {
+            for(size_t i = 0; i < m; i++)
+                y[i] = boost::math::owens_t(H[0], A[i]);
         }    
           
         // Return output
