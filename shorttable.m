497e4e83a300182829696ba9269c18d09ebf3a42
dhkehoe <dhkehoe@gmail.com>
2025-04-22 02:42:18 -0400

    updated to accept tables; backwards compatible

--- shorttable.m
+++ shorttable.m
@@ -1,48 +1,72 @@
 function y = shorttable(data,col,fun,varargin)
-% Create a short-format data table. First, input a (presumably raw) data
+% Create a short-format data table. First, input a 2D data matrix or a
 % table 'data' with observations across the rows and variables across the 
-% columns. Specify the outcome variable of interest 'col', where 'col' is a
-% logical vector to indicate which column hosts the outcome variable of
-% interest. Next, specify a function to apply to all instances of 
+% columns. Specify the outcome variable of interest 'col', where 'col'
+% indexes the outcome variable of interest (see below for indexing
+% instructions). Next, specify a function to apply to all instances of
 %   data(:,col)
 % in each marginalized cell of 'data'. Finally, provide all columns
 %   C1, ..., Cn
-% that will be used to parse 'data' into cells. Each column is also
-% specified as a logical vector to index 'data'. The size of the output
+% that will be used to parse 'data' into cells. Each 'C_i' argument is also
+% specified as a columnar index of 'data'. The size of the output
 % short-format table 'y', will be equal to the number of unique levels in
-% each of columns (factors) specified:
+% each of the factors specified:
 %   size(y) == [ numel(unique(data(:,C1))), ..., numel(unique(data(:,Cn)))]
 % Instances of 'nan' will be automatically discounted from consideration as
-% a level of factor Ci.
+% a level of factor 'Ci'.
 %
-% Alternatively, if 'fun' is numeric, 'y' will contain the proportion of
-% cases in each cell as a function of the number of cases across each level
-% of the factors, specified by their subscript, given in place of 'fun'.
+% Alternatively, if 'fun' is empty, 'y' will contain the count of cases in
+% in each cell. If 'fun' is a vector, 'y' will contain the proportion of
+% cases in each cell. The denominator of each proportion is specified by
+% the combinatoric number of cases across each factor specified in 'fun'.
+% The factors are ordered in the same order as they appear in arguments
+%   C1, ..., Cn.
 % See examples below.
+%
+% The indexing format for variables 'col' and C1, ..., Cn depends on the
+% format of the input data:
+%   (1) When 'data' is a table, indices must be a case-sensitive string
+%       specifying a variable in the table.
+%   (2) When 'data' is a matrix, indices must be a numeric (scalar) or
+%       logical (vector) columnar index of the matrix.
 %
 %--------------------------------------------------------------------------
 % USAGE
-%   y = sjtable(data,col,fun, C1,...,Cn);
+%   y = shorttable(data,col,fun, C1,...,Cn);
 %
 %--------------------------------------------------------------------------
 % EXAMPLES
 %   For all examples, imagine a matrix 'data', where each row is a trial,
-%   and it contains columns 'subject', 'correct', 'condition', and
-%   'outcome'. These variables are all of the same width as 'data'; e.g.,
-%       numel(subject) == size(data,2)
-%   and logically index a column of 'data'; e.g.,
+%   and the matrix contains variables 'subject', 'correct', 'condition',
+%   and 'outcome' arranged along the columns.
+%
+%   This can be formatted into a table:
+%       data.subject   % returns the subject number on every trial [1,..m]
+%
+%   This can also be formatted into a 2D matrix and indexed numerically:
+%       subject = 1;
+%       data(:,subject)   % returns the subject number on every trial [1,..m]
+%
+%   or logically:
+%       % numel(subject) == size(data,2)
+%       subject = [true, false, false, false];
+%       data(:,subject)   % returns the subject number on every trial [1,..m]
+%
+%   Regardless of the format utilized, imagine the variables are defined as
+%   follows:
 %       data(:,subject)   % returns the subject number on every trial [1,..m]
 %       data(:,correct)   % returns whether each trial was correct    {correct:1, incorrect:0}
 %       data(:,condition) % returns the condition on each trial       {condition1:1, condition2:2}
 %       data(:,outcome)   % returns some outcome on every trial       (e.g., response time)
-%   Furthermore, we can extract the unique levels in each factor such that
+%
+%   Now we can extract the unique levels in each factor such that
 %       sjs = unique(data(:,subject));   % [1,..m]
 %       cor = unique(data(:,correct));   % [0,1]
 %       con = unique(data(:,condition)); % [1,2]
 %
 %   _______
 %   CASE 1:
-%           y = sjtable(data,outcome,@mean,subject,condition);
+%           y = shorttable(data,outcome,@mean, subject,condition);
 %
 %       This gives an m by 2 matrix with the mean "outcome" of each subject
 %       in each condition. Thus
@@ -55,65 +79,112 @@ function y = shorttable(data,col,fun,varargin)
 %
 %   _______
 %   CASE 2:
-%           y = sjtable(data,[],[1],subject,correct);
+%           y = shorttable(data,[],[1], subject,correct);
 %
 %       This gives an m by 2 matrix with the proportion of [incorrect,correct]
 %       trials for each subject. C1,...,Cn determine the marginalized cells 
 %       in the numerator of the proportion, while 'fun' (i.e., [1]) 
-%       determines the demoninator of the proportion. Thus
+%       determines the marginalized cells of the demoninator of the
+%       proportion. Here, the [1] specifies that the denominator is the sum
+%       of cases for each subject (C1). Thus
 %
 %           y(i,j) == sum( data(:,subject)==sjs(i) & data(:,correct)==cor(j) ) / sum( data(:,subject)==sjs(i) )
 %
 %   _______
 %   CASE 3:
-%       When 'fun' is empty or is of equal length to the number of factors:
+%       A special case of the above is when 'fun' contains all factors,
+%       i.e., C1,...,Cn:
 %
-%           numel(fun)==n || isempty(fun)
+%           y = shorttable(data,[],[1,2],subject,correct);
+%       
+%       Here, the proportion in each cell is the proportion of the total
+%       number of cases in 'data'.
 %
-%       where 'n' is the number of factors in C1,...,Cn, then the
-%       demoninator in the proportion is simply the size of the data set:
-%
-%           y = sjtable(data,[],[1,2],subject,correct);
-%           y = sjtable(data,[],[],subject,correct);
-%
-%       These equvalently give an m by 2 matrix with the number of
-%       [incorrect,correct] trials for each subject as a proportion of the
-%       entire data set. Thus
 %           y(i,j) == sum( data(:,subject)==sjs(i) & data(:,correct)==cor(j) ) / size( data,2 )
 %
-%--------------------------------------------------------------------------
+%   _______
+%   CASE 4:
+%       When 'fun' is empty 
 %
+%           y = shorttable(data,[],[],subject,correct);
+%
+%       'y' simply contains counts of cases in each marginalized cell:
+%
+%           y(i,j) == sum( data(:,subject)==sjs(i) & data(:,correct)==cor(j) )
+%           
+%
+%--------------------------------------------------------------------------
+% HISTORY
+%   11/02/2024 - written
+%   22/04/2025 - updated to accept table; updated/fixed documentation
+
 %
 %   DHK - Feb. 11, 2024
+
 
 % Get the matrix size
 m = size(data);
 
-% Check that 'col' can logically index column of 'data'
-if numel(col) ~= m(2) && isa(fun,'function_handle')
-    error('When argument ''fun'' is a function handle, argument ''col'' must be a logical vector of equal length to the number of columns in ''data'' so to index a column of ''data''.');
+% Determine if input data is in table or matrix format
+tab = isa(data,'table');
+
+% Check that 'data' is either a 2D matrix or a table
+if ~( tab || isnumeric(data) && 2==numel(m) )
+    error('Argument ''data'' must be either a 2D matrix or a table.');
+end
+
+% Standardize the data format if a table was passed
+if tab
+    header = data.Properties.VariableNames; % Get the table header
+    data = table2array(data); % Convert to a matrix
+else
+    header = [];
+end
+
+% Check that 'col' can actually index some column of 'data', if using a
+% function to collapse the outcome variable 'col'
+if isa(fun,'function_handle')
+    col = index(col,m(2),header); % Convert to an index
+
+    % Check that there is a single index and it is valid (non-zero)
+    if ~( isscalar(col) && col )
+        error(sprintf([...
+            'When argument ''fun'' is a function handle, argument ''col'' must be one of the following:\n',...
+            '\t(1) When ''data'' is a table, ''col'' must be a case-sensitive string specifying a variable in the table.\n',...
+            '\t(2) When ''data'' is a matrix, ''col'' must be a numeric (scalar) or logical (vector) columnar index of the matrix.'...
+            ]));
+    end
 end
 
 % Get the number of factors
 n = numel(varargin);
 
+% Convert varargin to indices
+h = index(varargin,m(2),header);
+
 % Get the unique levels of each factor, validating as we go
 lvls = cell(1,n);
 for i = 1:n
-    % Check that the i_th factor can logically index column of 'data'
-    if numel(varargin{i}) ~= m(2)
-        error('Argument C%d in sjtable(data,col,fun, C1,...,Cn) has number of elements not equal to number of columns in ''data''.',i);
+    % Check that the i_th factor can index some column of 'data'
+    if ~h(i)
+        error(sprintf([...
+            'Argument ''C%d'' is misformatted to index a column in ''data'' during function call\n',...
+            '\tshorttable(data,col,fun, C1,...,Cn)\n\n',...
+            'Use the following format for arguments C1,...,Cn:\n',...
+            '\t(1) When ''data'' is a table, ''Ci'' must be a case-sensitive string specifying a variable in the table.\n',...
+            '\t(2) When ''data'' is a matrix, ''Ci'' must be a numeric (scalar) or logical (vector) columnar index of the matrix.'...
+            ],i)); %#ok
     end
 
     % Get the unique levels
-    lvls{i} = unique(data(:,varargin{i}));
+    lvls{i} = unique(data(:,h(i)));
     lvls{i}( isnan(lvls{i}) ) = []; % Trim out NaNs
 end
 
 %% Compute subject table
 ind = ones(1,n); % Current level across factors
 sz = cellfun(@numel,lvls); % Size of all factors
-if numel(sz)==1, sz = [sz,1]; end % Protect against vectors
+if isscalar(sz), sz = [sz,1]; end % Protect against vectors
 y = nan(sz); % Output matrix
 
 % Linearly step through 'y'
@@ -123,7 +194,7 @@ for i = 1:numel(y)
     % Step through factors
     for j = 1:n
         % Index data where the j_th factor is equal to the k_th level, where k = ind(j)
-        idx(:,j) = data(:,varargin{j})==lvls{j}(ind(j));
+        idx(:,j) = data(:,h(j))==lvls{j}(ind(j));
     end
 
     % Apply the function '@fun' to the indexed data in the column specified
@@ -159,5 +230,38 @@ for i = 1:numel(y)
         % And so on, checking all factors, until there are no additional
         % factors to increment (terminate on j<n)
         j=j+1;
+    end
+end
+
+%% Utility
+function idx = index(x,m,header)
+% Convert various indices to a common format (numeric indices)
+
+% Ensure x is always a cell array
+if ~iscell(x)
+    x = {x};
+end
+
+% Initialize output
+n = numel(x);
+idx = zeros(1,n);
+
+for i = 1:n % Step through x
+
+    % Convert table headers to logical indices
+    if ~isempty(header)
+        x{i} = strcmp(header,x{i});
+    end
+
+    % Convert logical indices to numeric indices
+    if islogical(x{i})
+        x{i} = find(x{i});
+    end
+
+    % Check that it is a valid numeric index
+    if isnumeric(x{i}) && isscalar(x{i}) && ~mod(x{i},1) && 1<=x{i} && x{i}<=m
+        idx(i) = x{i};
+    else
+        idx(i) = 0; % Invalid
     end
 end
