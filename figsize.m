3bae3b0c00fea74fcb349719a1855230b6871e86
dhkehoe <dhkehoe@gmail.com>
2025-04-29 02:44:44 -0400

    updated so that you can min/max the figure window pane

--- figsize.m
+++ figsize.m
@@ -1,7 +1,18 @@
-function figsize(pos)
+function figsize(pos,state)
+if nargin<2
+    state = [];
+end
+if nargin<1 || isempty(pos)
+    pos = get(gcf,'OuterPosition');
+end
+
 units = get(gcf,'Units');
 try
     set(gcf,'Units','normalized','OuterPosition',pos);
+    if ~isempty(state)
+        states = {'normal','maximized','minimized','fullscreen'};
+        set(gcf,'WindowState',states{find(contains(states,state),1,'first')});
+    end
 catch err
     error(struct('identifier',err.identifier,'message',err.message,'stack',err.stack(end)));
 end
