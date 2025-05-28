49c103d736fad91d2efcf367118b01803da82c88
dhkehoe <dhkehoe@gmail.com>
2025-05-28 15:39:15 -0400

    better error handling

--- fig2pdf.m
+++ fig2pdf.m
@@ -3,19 +3,26 @@ function fig2pdf(filename,h,dpi)
 % with no whitespace padding. Use the resolution (dots-per-inch) specified
 % by the numeric scalar 'dpi' (default = 300).
 
-if nargin<3
+if nargin<3 || isempty(dpi)
     dpi = 300;
-end
-if nargin<2
-    h = gcf;
+elseif ~( isnumeric(dpi) || isscalar(dpi) )
+    error('Optional argument ''dpi'' must be a numeric integer specifying the image resolution in units of dots-per-inch.');
 end
 
+if nargin<2 || isempty(h)
+    h = gcf;
+elseif ~isfigure(h)
+    error('Optional argument ''h'' must be a figure handle.');
+end
+
+if numel(filename)<4 || ~strcmp(filename(end-3:end),'.pdf')
+    filename = [filename,'.pdf'];
+end
+
+%%
 set(h,'Units','Inches');
 pos = get(h,'Position');
 set(h,'Renderer','painters','PaperUnits','Inches','PaperPositionMode','Manual',...
     'PaperPosition',[0,0,pos(3),pos(4)],...
     'PaperSize',[pos(3),pos(4)]);
-if numel(filename)<4 || ~strcmp(filename(end-3:end),'.pdf')
-    filename = [filename,'.pdf'];
-end
 print(h,filename,'-dpdf',['-r',num2str(dpi)]);
