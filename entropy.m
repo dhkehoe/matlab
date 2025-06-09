6eb4d52a49ae166912045d07109672ef1a038b45
dhkehoe <dhkehoe@gmail.com>
2025-06-09 19:07:22 -0400

    allow for multi-dimensional arrays

--- entropy.m
+++ entropy.m
@@ -1,14 +1,26 @@
-function y = entropy(p,n)
-if nargin<2
-    n = [];
-end
+function H = entropy(p,b,dim)
+% Compute the cross entropy of some array 'p', along the dimension 'dim',
+% using the logarithm of base 'b'.
 
-if isempty(n)
-    lp = log(p);
+% Use the specified base
+if nargin<2 || isempty(b)
+    lp = log(p); % Default to natural log
 else
-    lp = logn(p,n);
+    lp = logn(p,b);
 end
 
+% Recode -Inf as 0, which
+%   (1) avoids needing to use nansum()
+%   (2) ensures that if
+%           all(isnan(p(i,:)))==1
+%       then
+%           isnan(H(i))==1
 lp(isinf(lp)) = 0;
 
-y = -sum(p.*lp);
+% Sum over appropriate dimension, allowing for n-dimensional arrays
+if nargin<3 || isempty(dim)
+    H = -sum(p.*lp); % Let MATLAB decide which dimension to use
+else
+    H = -sum(p.*lp,dim);
+end
+
