73dab000284dab5959f7a0b36309dc8884fb3151
dhkehoe <dhkehoe@gmail.com>
2026-04-16 03:01:36 -0400

    added an equality tolerance argument

--- equal.m
+++ equal.m
@@ -1,13 +1,20 @@
-function y = equal(x,dim)
+function y = equal(x,dim,tol)
 % Check whether all elements in 'x' are equal along the dimension specified
 % by 'dim'. Consistent with MATLAB default behavior, the default dimension
 % for matrices is 1, and the non-zero sized dimension for vectors.
 
+if nargin<3
+    tol = [];
+end
 if nargin<2
     dim = [];
 end
 try
-    y = logical(collapsedim(x,@iseq,dim));
+    if isempty(tol)
+        y = logical(collapsedim(x,@iseq,dim));
+    else
+        y = logical(collapsedim(x,@(x)iseqtol(x,tol),dim));
+    end
 catch err
     error(struct('identifier',err.identifier,'message',err.message,'stack',err.stack(end)));
 end
@@ -17,4 +24,10 @@ function b = iseq(x)
 b = true;
 for i = 2:numel(x)
     b = b && x(i) == x(i-1);
+end
+
+function b = iseqtol(x,tol)
+b = true;
+for i = 2:numel(x)
+    b = b && abs(x(i) - x(i-1)) <= tol;
 end
