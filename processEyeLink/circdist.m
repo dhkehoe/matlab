290835b66e8d561796784f12533a74f7e5697488
dhkehoe <dhkehoe@gmail.com>
2026-04-16 02:57:45 -0400

    added dim argument

--- processEyeLink/circdist.m
+++ processEyeLink/circdist.m
@@ -1,8 +1,15 @@
-function dist = circdist(a1,a2,rad)
-% Utility for computing circular distance between 2 sets of angles. This
-% distance is bounded on [0,pi]. Optional argument 'rad' to specify whether
-% the angles are in radians (true | default) or in degrees (false).
+function dist = circdist(a1,a2,rad,dim)
+% Utility for computing absolute circular distance between 2 sets of
+% angles in 'a1' and 'a2'. 'a1' and 'a2' must match in size or at least one
+% of them must be scalar. Circular distances are bounded on [0,pi].
+% Optional argument 'rad' specifies whetherthe angles are in radians (true)
+% or in degrees (false) (default = true). Optional argument 'dim' specifies
+% the dimension of 'a1' and 'a2' along which to perform the computation.
 
+% Default to no dimension argument
+if nargin<4
+    dim = [];
+end
 % Default to radians
 if nargin<3 || isempty(rad) || rad
     c = 2*pi;
@@ -12,7 +19,11 @@ end
 
 % Compute the circular distances
 try
-    dist = min(mod(a1-a2,c),mod(a2-a1,c));
+    if isempty(dim)
+        dist = min(mod(a1-a2,c),mod(a2-a1,c));
+    else
+        dist = min(mod(a1-a2,c),mod(a2-a1,c),dim);
+    end
 catch err % Probably a size mismatch, catch and rethrow from here
     error(struct('identifier',err.identifier,'message',err.message,'stack',err.stack(end)));
 end
