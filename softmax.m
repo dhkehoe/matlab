9067a4cc9f502f039301c40ddb6e2ba4c4c73c0c
dhkehoe <dhkehoe@gmail.com>
2026-04-21 19:59:05 -0400

    fixed an obvious computational issue

--- softmax.m
+++ softmax.m
@@ -1,10 +1,22 @@
 function y = softmax(x,lambda,dim)
+
+% Verify lambda argument
 if nargin<2 || isempty(lambda)
     lambda = 1;
+elseif ~isnumeric(x)
+    error('Argument ''lambda'' must be numeric.');
 end
+
+% Define exponential of x
+y = exp(x.*lambda);
+
+% Let MATLAB pick the default dimension to sum over
 if nargin<3 || isempty(dim)
-    % Let MATLAB pick the default dimension
-    y = exp(x) ./ sum(exp(x.*lambda));
+    y = y./sum(y);
 else
-    y = exp(x) ./ sum(exp(x.*lambda),dim);
+    try % Catch any misformatting errors thrown by sum() and rethrow them from here
+        y = y./sum(y,dim);
+    catch err
+        error(struct('identifier',err.identifier,'message',err.message,'stack',err.stack(end)));
+    end
 end
