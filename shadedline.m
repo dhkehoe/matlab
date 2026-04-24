42e92a8e7335a169b5e50d9ee34e52547c688fb4
dhkehoe <dhkehoe@gmail.com>
2026-04-24 02:12:34 -0400

    exhausted formatting params. made the input handling much less janky

--- shadedline.m
+++ shadedline.m
@@ -1,51 +1,111 @@
 function varargout = shadedline(x,y,e,varargin)
 
+% Force these to row vectors
 x = x(:)';
 y = y(:)';
-% e = e(:);
 
-hold on;
+% Set hold behavior
+ish = ishold;
 
-varargin0 = {'k','LineStyle','none','Marker','none'};
-i = 1; ex = 0;
-while i < numel(varargin)
-    j = i-ex;
-    if any(strcmpi(varargin{j},{...
-            'FaceAlpha',...
-            'FaceColor',...
-            'EdgeAlpha',...
-            'EdgeColor',...
-            }))
+% Be sure to wipe old plot if hold is off
+if ~ish
+    clf;
+end
 
-        varargin0 = [varargin0, varargin(j:j+1)]; %#ok
-        varargin(j:j+1) = [];
+%% Retrieve optional arguments that cannot be passed to plot()
+try
+    % keyboard
+    % Parse varargin for anything that won't be accepted by plot()
+    [varargin,  fa ] = inputChecker(varargin,'FaceAlpha',         .1 );
+    [varargin,  fc ] = inputChecker(varargin,'FaceColor',         [] );
+    [varargin,  ea ] = inputChecker(varargin,'EdgeAlpha',         [] );
+    [varargin,  ec ] = inputChecker(varargin,'EdgeColor',         [] );
+    [varargin, els ] = inputChecker(varargin,'EdgeLineStyle',     [] );
+    [varargin, elw ] = inputChecker(varargin,'EdgeLineWidth',     [] );
+    [varargin,  em ] = inputChecker(varargin,'EdgeMarker',        [] );
+    [varargin, ems ] = inputChecker(varargin,'EdgeMarkerSize',    [] );
+    [varargin, emc ] = inputChecker(varargin,'EdgeMarkerColor',   [] );
+    
+    % Check that any additional arguments are valid when passed directly to
+    % plot(); let plot() catch offenders
+    plot(nan,nan,varargin{:});
 
-        i=i+1;
-        ex = ex+2;
+    % Fill will annoyingly crash without this argument
+    varargin0 = {'k'};  %,''Marker','none'};
+
+    % Input the lengthly list of optionals manually...
+    if ~isempty(fa)
+        varargin0 = [varargin0, 'FaceAlpha', fa];
+    end
+    if ~isempty(fc)
+        varargin0 = [varargin0, 'FaceColor', fc];
+    end
+    if ~isempty(ea)
+        varargin0 = [varargin0, 'EdgeAlpha', ea];
+    end
+    if ~isempty(ec)
+        varargin0 = [varargin0, 'EdgeColor', ec];
+    end
+    if ~isempty(els)
+        varargin0 = [varargin0, 'LineStyle', els];
+    end
+    if ~isempty(elw)
+        varargin0 = [varargin0, 'LineWidth', elw];
+    end
+    if ~isempty(em)
+        varargin0 = [varargin0, 'Marker', em];
+    end
+    if ~isempty(ems)
+        varargin0 = [varargin0, 'MarkerSize', ems];
+    end
+    if ~isempty(emc)
+        varargin0 = [varargin0, 'MarkerFaceColor', emc, 'MarkerEdgeColor', emc];
     end
 
-    i=i+1;
+    % If EdgeColor and EdgeLineStyle aren't provided, default to no line,
+    % overriding the default, which is a black line
+    if isempty(ec) && isempty(els) && isempty(elw)
+        varargin0 = [varargin0, 'LineStyle', 'none'];
+    end
+
+    % Check that the arguments specifically targeting fill() are valid
+    % when passed; let fill() catch offenders
+    fill(nan,nan,varargin0{:});    
+
+catch err
+    throwAsCaller(err);
 end
 
-% Set default FaceAlpha property
-if ~strcmpi('FaceAlpha',varargin0)
-    varargin0 = [varargin0, 'FaceAlpha', .1];
+
+%% Plot
+try
+    hold on;
+
+    % Plot line
+    h = plot(x,y,varargin{:});
+
+    % If FaceColor isn't provided, default to whatever color is used for the
+    % line
+    if ~strcmpi('FaceColor',varargin0)
+        varargin0 = [varargin0, 'FaceColor', h.Color];
+    end
+
+    % Plot shading
+    if isvector(e)
+        fill([x,fliplr(x)],[y-e(:)',fliplr(y+e(:)')],varargin0{:});
+    else
+        fill([x,fliplr(x)],[e(1,:),fliplr(e(2,:))],varargin0{:});
+    end
+
+catch err
+    % Likely bad arguments for plot()
+    throwAsCaller(err);
 end
 
-%%
-h = plot(x,y,varargin{:});
-
-% If FaceColor isn't provided, default to whatever color is used for the
-% line
-if ~strcmpi('FaceColor',varargin0)
-    varargin0 = [varargin0, 'FaceColor', h.Color];
-end
-if isvector(e)
-    fill([x,fliplr(x)],[y-e(:)',fliplr(y+e(:)')],varargin0{:});
-else
-    fill([x,fliplr(x)],[e(1,:),fliplr(e(2,:))],varargin0{:});
-end
+%% Wrap up
 
 if nargout
     varargout{1} = h;
 end
+
+hold(ish);
