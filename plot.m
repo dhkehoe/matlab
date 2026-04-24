8ddaa3ff995ab31b0462253f5c9ea307fb277f65
dhkehoe <dhkehoe@gmail.com>
2026-04-24 00:51:12 -0400

    updated the doc to instruct users how to turn off the persistent annoying overload messages...

--- plot.m
+++ plot.m
@@ -1,6 +1,12 @@
 function varargout = plot(varargin)
 % Overload the plot() function so that I can include a line alpha argument,
 % which MATLAB desperately needs.
+% 
+% NOTE: The mere existence of this .m file on your file path will give
+% persistent annoying warning messages anytime you save another .m file on
+% that path. You can suppress them by creating (or modifying) a file
+% specifically called "startup.m" on the same path and adding this line:
+%   warning('off', 'MATLAB:dispatcher:nameConflict');
 
 % Add any additional wishlist args here...
 [varargin, a] = inputChecker(varargin,'linealpha',[],@(x)isnumeric(x)&&isscalar(x)&&0<=x&&x<=1,...
