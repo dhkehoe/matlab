f5b6e9bcd36fb0ab885acf99d14de5cef90d4a9e
dhkehoe <dhkehoe@gmail.com>
2026-04-24 00:35:38 -0400

    overload plot because i'm sick of setting line alpha manually

---
+++ plot.m
@@ -0,0 +1,24 @@
+function varargout = plot(varargin)
+% Overload the plot() function so that I can include a line alpha argument,
+% which MATLAB desperately needs.
+
+% Add any additional wishlist args here...
+[varargin, a] = inputChecker(varargin,'linealpha',[],@(x)isnumeric(x)&&isscalar(x)&&0<=x&&x<=1,...
+    'Optional argument ''LineAlpha'' must be a numeric scalar in the interval (0,1).');
+
+% Invoke the MATLAB executable plot()
+try
+    h = builtin('plot',varargin{:});
+catch err
+    throwAsCaller(err);
+end
+
+% Utilize the line alpha argument
+for i = 1:numel(h) * ~isempty(a)
+    h(i).Color(4) = a;
+end
+
+% Return handle if necessary
+if nargout
+    varargout = {h};
+end
